89ee0d8cf3246ecfa1929d1e53af55e331602f71
Philip Arvidsson <philip@philiparvidsson.com>
2016-11-19 22:47:09 +0100

    further improved make scripts

--- make.py
+++ make.py
@@ -3,74 +3,41 @@ import os, sys
 sys.path.insert(0, os.path.join('build', 'pymake'))
 from pymake import *
 
-NAME = 'Pong.exe'
-
-FLAGS = ['/nologo',
-         '/o',
-         '/platform:x64',
-         '/target:winexe']
-
-LIBS = ['PresentationCore.dll',
-        'System.IO.dll',
-        'System.Runtime.dll',
-        'WindowsBase.dll',
-
-        'SharpDX.D3DCompiler.dll',
-        'SharpDX.DXGI.dll',
-        'SharpDX.Direct3D11.dll',
-        'SharpDX.Mathematics.dll',
-        'SharpDX.XAudio2.dll',
-        'SharpDX.dll']
-
-LIBDIRS = [r'C:\Windows\Microsoft.NET\Framework64\v4.0.30319',
-           r'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\WPF',
-           r'lib\SharpDX']
-
-BINDIR = 'bin'
-SRCDIR = 'src'
-
-CSC = r'C:\Program Files (x86)\MSBuild\14.0\Bin\csc.exe'
-
-#---------------------------------------
-# FUNCTIONS
-#---------------------------------------
+import csc
 
 @target
 @depends_on('compile', 'content', 'libs')
-def all():
+def all(conf):
     pass
 
 @target
-def clean():
-    if os.path.isdir(BINDIR):
-        remove_dir(BINDIR)
+def content(conf):
+    copy('res', os.path.join(conf.bindir, 'Content'))
 
 @target
-def content():
-    copy('res', os.path.join(BINDIR, 'Content'))
+def libs(conf):
+    copy(r'lib\SharpDX', conf.bindir, '*.dll')
 
-@target
-def libs():
-    copy('lib/SharpDX', BINDIR, '*.dll')
+pymake(csc.defaultConf(), {
+    'name': 'Pong.exe',
 
-@target
-def compile():
-    create_dir(BINDIR)
+    'flags': ['/target:winexe',
+              #'/debug',
+              #'/define:DEBUG',
+              '/o',
+              '/platform:x64'],
 
-    libdirs = ['/lib:' + ','.join(LIBDIRS)]
-    libs    = ['/r:' + lib for lib in LIBS]
-    out     = ['/out:' + os.path.join(BINDIR, NAME)]
-    sources = ['/recurse:' + os.path.join(SRCDIR, '*.cs')]
+    'libs': ['PresentationCore.dll',
+             'System.IO.dll',
+             'System.Runtime.dll',
+             'WindowsBase.dll',
 
-    run_program(CSC, FLAGS + libdirs + libs + out + sources)
+             'SharpDX.D3DCompiler.dll',
+             'SharpDX.DXGI.dll',
+             'SharpDX.Direct3D11.dll',
+             'SharpDX.Mathematics.dll',
+             'SharpDX.XAudio2.dll',
+             'SharpDX.dll'],
 
-@target
-def run():
-    os.chdir(BINDIR)
-    run_program(NAME)
-
-#---------------------------------------
-# SCRIPT
-#---------------------------------------
-
-pymake()
+    'libdirs': [r'lib\SharpDX'],
+})
