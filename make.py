02c01367747b51636211c9facbba5058de2f6e9d
Philip Arvidsson <philip@philiparvidsson.com>
2016-11-19 19:51:46 +0100

    improved make scripts

--- make.py
+++ make.py
@@ -1,94 +1,75 @@
-#---------------------------------------
-# IMPORTS
-#---------------------------------------
-
-import os
-import sys
-
+import os, sys
 sys.path.insert(0, os.path.join('build/pymake'))
 from pymake import *
 
-#---------------------------------------
-# CONSTANTS
-#---------------------------------------
+NAME = 'Pong.exe'
 
-CSC = 'C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\csc.exe'
+FLAGS = ['/nologo',
+         '/o',
+         '/platform:x64',
+         '/target:winexe']
 
-FLAGS = [
-#    '/debug',
-#    '/define:DEBUG',
-    '/nologo',
-    '/o',
-    '/platform:x64',
-    '/target:winexe'
-]
+LIBS = ['PresentationCore.dll',
+        'System.IO.dll',
+        'System.Runtime.dll',
+        'WindowsBase.dll',
 
-TARGET = 'Pong.exe'
+        'SharpDX.D3DCompiler.dll',
+        'SharpDX.DXGI.dll',
+        'SharpDX.Direct3D11.dll',
+        'SharpDX.Mathematics.dll',
+        'SharpDX.XAudio2.dll',
+        'SharpDX.dll']
 
-LIBS = [
-    'PresentationCore.dll',
-    'System.IO.dll',
-    'System.Runtime.dll',
-    'WindowsBase.dll',
-
-    'SharpDX.D3DCompiler.dll',
-    'SharpDX.DXGI.dll',
-    'SharpDX.Direct3D11.dll',
-    'SharpDX.Mathematics.dll',
-    'SharpDX.XAudio2.dll',
-    'SharpDX.dll'
-]
+LIBDIRS = [r'C:\Windows\Microsoft.NET\Framework64\v4.0.30319',
+           r'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\WPF',
+           r'lib\SharpDX']
 
 BINDIR = 'bin'
 SRCDIR = 'src'
 
-LIBDIRS = [
-    'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\',
-    'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\WPF',
-    'lib\\SharpDX'
-]
+CSC = r'C:\Program Files (x86)\MSBuild\14.0\Bin\csc.exe'
 
 #---------------------------------------
 # FUNCTIONS
 #---------------------------------------
 
-@target('compile', 'content', 'libs')
+@target
+@depends_on('compile', 'content', 'libs')
 def all():
     pass
 
-@target()
+@target
 def clean():
     if os.path.isdir(BINDIR):
         remove_dir(BINDIR)
 
-@target()
+@target
 def content():
     copy('res', os.path.join(BINDIR, 'Content'))
 
-@target()
+@target
 def libs():
     copy('lib/SharpDX', BINDIR, '*.dll')
 
-@target()
+@target
 def compile():
-    if not os.path.exists(BINDIR):
-        os.mkdir(BINDIR)
+    create_dir(BINDIR)
 
-    flags   = FLAGS
     libdirs = ['/lib:' + ','.join(LIBDIRS)]
-    libs    = ['/r:' + s for s in LIBS]
-    out     = ['/out:' + os.path.join(BINDIR, TARGET)]
+    libs    = ['/r:' + lib for lib in LIBS]
+    out     = ['/out:' + os.path.join(BINDIR, NAME)]
     sources = ['/recurse:' + os.path.join(SRCDIR, '*.cs')]
 
-    run_cmd(CSC, flags + libdirs + libs + out + sources)
+    run_program(CSC, FLAGS + libdirs + libs + out + sources)
 
-@target()
+@target
 def run():
     os.chdir(BINDIR)
-    subprocess.call([TARGET])
+    run_program(TARGET)
 
 #---------------------------------------
 # SCRIPT
 #---------------------------------------
 
-make()
+pymake()
